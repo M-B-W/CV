c8b89b0a845d44c7c795fec5a2ecde973e216915
Apollo Blaze <srichandsureshrocks@gmail.com>
2024-09-26 00:10:09 +0530

    build test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#ffffff]' />
@@ -16,11 +16,18 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className={`${styles.heroHeadText} `} style={{ color: '#a82323' }}>Srichand Suresh</span>
+            Hi, I'm{" "}
+            <span className={`${styles.heroHeadText}`} style={{ color: '#a82323' }}>
+              <Typical
+                steps={["Srichand Suresh", 2000]} // Typing effect for your name
+                loop={1}
+                wrapper="span"
+              />
+            </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Exploring data science and <br className='sm:block hidden' />
-          automating lives.
+            Exploring data science and <br className='sm:block hidden' />
+            automating lives.
           </p>
         </div>
       </div>
