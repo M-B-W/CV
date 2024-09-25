9502aac37581774920e1383ca099ba14aa9952ee
Apollo Blaze <srichandsureshrocks@gmail.com>
2024-09-25 23:37:52 +0530

    Updated theme and home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,72 @@
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const name = "Srichand Suresh";
+  const [displayedText, setDisplayedText] = useState("");
+  const typingSpeed = 50; // Typing speed in milliseconds
+  const deletingSpeed = 100; // Deleting speed in milliseconds
+  const pauseBetweenLoops = 800; // Pause after typing
+
+  useEffect(() => {
+    let typingTimeout;
+    let deletingTimeout;
+
+    const typeText = () => {
+      for (let i = 0; i <= name.length; i++) {
+        typingTimeout = setTimeout(() => {
+          setDisplayedText(name.substring(0, i));
+        }, typingSpeed * i);
+      }
+
+      typingTimeout = setTimeout(() => {
+        deleteText();
+      }, typingSpeed * name.length + pauseBetweenLoops);
+    };
+
+    const deleteText = () => {
+      for (let i = name.length; i >= 0; i--) {
+        deletingTimeout = setTimeout(() => {
+          setDisplayedText(name.substring(0, i));
+        }, deletingSpeed * (name.length - i));
+      }
+
+      deletingTimeout = setTimeout(() => {
+        typeText();
+      }, deletingSpeed * (name.length + 1) + pauseBetweenLoops);
+    };
+
+    typeText();
+
+    return () => {
+      clearTimeout(typingTimeout);
+      clearTimeout(deletingTimeout);
+    };
+  }, []);
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#ffffff]' />
+          <div className='w-1 sm:h-80 h-40 white-gradient' />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
-          </h1>
+        <h1 className={`${styles.heroHeadText} text-white`}>
+  <span className="text-4xl">Hi, I'm</span>{" "} {/* Adjust font size here */}
+  <span className={`${styles.heroHeadText}`} style={{ color: '#a82323' }}>
+    {displayedText}
+  </span>
+</h1>
+
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Exploring data science and <br className='sm:block hidden' />
+            automating lives.
           </p>
         </div>
       </div>
