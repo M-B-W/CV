178531b7fda9f6a346b06b4a3cb90626483e7268
Apollo Blaze <srichandsureshrocks@gmail.com>
2024-09-26 00:22:47 +0530

    build test-3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { TypeAnimation } from "react-type-animation"; // New import
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -7,33 +7,20 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#ffffff]' />
-          <div className='w-1 sm:h-80 h-40 white-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm{" "}
-            <span className={`${styles.heroHeadText}`} style={{ color: '#a82323' }}>
-              <TypeAnimation
-                sequence={[
-                  "Srichand Suresh", // Your name
-                  2000,               // Delay in ms
-                  "",                 // Optional: Clear name
-                  2000,               // Delay before retyping
-                ]}
-                wrapper="span"
-                cursor={true}
-                repeat={Infinity} // Loop the animation
-              />
-            </span>
+            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Exploring data science and <br className='sm:block hidden' />
-            automating lives.
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
           </p>
         </div>
       </div>
