adc405c1e2fd484d2f5b4af7c1fcff0d8c1baa9f
Apollo-Blaze <srichandsureshrocks@gmail.com>
2024-09-29 09:41:37 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import React, { Suspense } from 'react';
+
+// Lazy load StarsCanvas
+const StarsCanvas = React.lazy(() => import('./StarsCanvas'));
 
 const App = () => {
   return (
     <BrowserRouter>
-
-      <div className='relative z-0 bg-primary '>
+      <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
+
         <div className='relative z-0'>
-        <About />
-        <StarsCanvas />
+          <About />
+          {/* Wrap with Suspense and add a fallback for loading */}
+          <Suspense fallback={<div>Loading stars...</div>}>
+            <StarsCanvas />
+          </Suspense>
         </div>
+        
         <Experience />
-          <Works />
+        <Works />
+
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+          <Suspense fallback={<div>Loading stars...</div>}>
+            <StarsCanvas />
+          </Suspense>
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
