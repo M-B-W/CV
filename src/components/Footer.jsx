ddddec2d8d9403f755c46d1d4dac7bc8bf06629a
Bharathwaj.M <78369066+M-B-W@users.noreply.github.com>
2024-12-31 22:14:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
         <div className="flex flex-col items-center gap-3">
           <span className='text-blue-500'> Email: <a href="mailto:bharathwajprofessional04@gmail.com" className="text-white hover:text-red-500">bharathwajprofessional04@gmail.com</a></span>
           <span className='text-blue-500'> Github: <a href="https://github.com/M-B-W" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-500">M-B-W</a></span>
-          <span className='text-blue-500'> LinkedIn: <a href="www.linkedin.com/in/bharathwaj-darkangel0011" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-500">Bharathwaj M</a></span>
+          <span className='text-blue-500'> LinkedIn: <a href="https://www.linkedin.com/in/bharathwaj-darkangel0011/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-500">Bharathwaj M</a></span>
           <span className='text-blue-500'> Instagram: <a href="https://www.instagram.com/_bharath_waj_04/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-red-500">_bharath_waj_04</a></span>
           <span className='text-blue-500'> 
             Phone: <a href="tel:+916385790363" className="text-white hover:text-red-500">+91-6385790363</a>
